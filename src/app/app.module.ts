dc1e54766889c4c4a01d7819e449c54d7c142dc5
Yves Vanduynslager <yves.vanduynslager@telenet.be>
2017-09-27 20:11:22 +0200

    westrijd en speler componenten verwijderd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SpelerComponent } from './speler/speler.component';
-import { WedstrijdComponent } from './wedstrijd/wedstrijd.component';
 import { SpelerDetailComponent } from './speler-detail/speler-detail.component';
 import { WedstrijdDetailComponent } from './wedstrijd-detail/wedstrijd-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpelerComponent,
-    WedstrijdComponent,
     SpelerDetailComponent,
     WedstrijdDetailComponent
   ],
--- src/app/wedstrijd-detail/wedstrijd-detail.component.ts
+++ src/app/wedstrijd-detail/wedstrijd-detail.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-wedstrijd-detail',
   templateUrl: './wedstrijd-detail.component.html',
   styleUrls: ['./wedstrijd-detail.component.css']
 })
 export class WedstrijdDetailComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
